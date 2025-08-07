e60ab72bad8bb50ca947bf1c559a21676aff89c1
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-08-07 21:37:04 +0530

    Add frontend subproject as a new directory

---
+++ Backend/routes/generateRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios')
+
+router.post('/', async (req, res) => {
+    const {prompt} = req.body;
+
+    try {
+        await axios.post()
+    } catch (error) {
+        
+    }
+})
