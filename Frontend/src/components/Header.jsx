8fd20c8cb3f68312a03c7c71eb296adbf6fc3c44
Mohd Meraj <mohammadmerajansari352@gmail.com>
2025-08-09 12:35:07 +0530

    Update Header and Hero components, add new styles, and create empty page files for About, Contact, and Service

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -4,14 +4,13 @@ const Header = () => {
   return (
     <div className=' bg-black flex justify-between items-center px-20 py-4'>
         <div>
-            <h1 className='text-white text-2xl font-bold'>AI Email Generator</h1>
+            <h1 className='text-white text-2xl font-bold'>Serendale</h1>
         </div>
         <div>
-        <ul className='flex space-x-4 text-white'>
-            <li>Service</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Footer</li>
+        <ul className='md:flex  space-x-4 text-white'>
+            <li className='cursor-pointer'>Service</li>
+            <li className='cursor-pointer'>About</li>
+            <li className='cursor-pointer'>Contact</li>
             {/* <li>Login</li> */}
         </ul>
         </div>
--- Frontend/src/components/Hero.jsx
+++ Frontend/src/components/Hero.jsx
@@ -13,17 +13,17 @@ const Hero = () => {
       </div>
 
       <div className="flex flex-col justify-center items-center text-center h-full px-4 z-10 relative">
-        <h1 className="text-5xl md:text-6xl font-bold text-white">
-          Generate Emails Instantly with AI
+        <h1 className="bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 bg-clip-text text-transparent text-5xl md:text-6xl font-bold hero_header text-white">
+          Generate Emails Instantly with  <span className='text-5xl AI ordinal'>AIst</span>
         </h1>
-        <p className="mt-4 text-lg md:text-xl text-gray-300 max-w-2xl">
+        <p className="mt-4 text-sm hero_content text-gray-300">
           Write professional, personalized emails in seconds — no typing needed.
         </p>
-        <button className="mt-6 px-6 py-3 bg-blue-600 text-white font-semibold rounded-lg hover:bg-blue-700 transition">
-          Try It Now
-        </button>
+        {/* <button className="mt-6 px-6 py-3 bg-blue-600 cursor-pointer text-white font-semibold rounded-lg hover:bg-blue-700 transition">
+          SERVICES
+        </button> */}
+      
       </div>
-
       {/* Optional: dark overlay for better text visibility */}
       <div className="absolute inset-0 bg-black/50 z-0" />
     </div>
